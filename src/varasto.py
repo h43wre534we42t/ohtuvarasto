94a98cf2b2f3521fd91b6f36f5d63ffc09ec467b
Timur <tmandziev@gmail.com>
2024-11-04 20:53:26 +0200

    fixed varasto.py

--- src/varasto.py
+++ src/varasto.py
@@ -42,5 +42,5 @@ class Varasto:
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaaa {self.paljonko_mahtuu()}"
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
         
