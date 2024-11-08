a32b9135fa6ec05950db207a8d65e47ddbe65757
Timur <tmandziev@gmail.com>
2024-11-08 14:40:50 +0200

    tehtävä3

--- src/index.py
+++ src/index.py
@@ -1,58 +1,43 @@
 from varasto import Varasto
 
-
 def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
-    print("Luonnin jälkeen:")
-    print(f"Mehuvarasto: {mehua}")
-    print(f"Olutvarasto: {olutta}")
+    print(f"Luonnin jälkeen:\nMehuvarasto: {mehua}\nOlutvarasto: {olutta}\n")
 
-    print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
+    print(f"Olut getterit:\nsaldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
+    print(f"npaljonko_mahtuu = {olutta.paljonko_mahtuu()}\n")
 
+    part2(part1(mehua, olutta))
+def part1(mehua, olutta):
     print("Mehu setterit:")
-    print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
-    print(f"Mehuvarasto: {mehua}")
-    print("Otetaan 3.14")
+    print(f"Lisätään 50.7\nMehuvarasto: {mehua}")
     mehua.ota_varastosta(3.14)
-    print(f"Mehuvarasto: {mehua}")
+    print(f"Otetaan 3.14\nMehuvarasto: {mehua}\n")
 
     print("Virhetilanteita:")
-    print("Varasto(-100.0);")
-    huono = Varasto(-100.0)
-    print(huono)
+    print(f"Varasto(-100.0):\n{Varasto(-100.0)}")
+    print(f"Varasto(100.0, -50.7):\n{Varasto(100.0, -50.7)}\n")
+    tupl = (mehua, olutta)
+    return tupl
 
-    print("Varasto(100.0, -50.7)")
-    huono = Varasto(100.0, -50.7)
-    print(huono)
-
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.lisaa_varastoon(1000.0)")
+def part2(tupl):
+    mehua = tupl[0]
+    olutta = tupl[1]
     olutta.lisaa_varastoon(1000.0)
-    print(f"Olutvarasto: {olutta}")
+    print(f"Lisätään 1000.0 olueen\nOlutvarasto: {olutta}\n")
 
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
-    print(f"Mehuvarasto: {mehua}")
+    print(f"Lisätään -666.0 mehuun\nMehuvarasto: {mehua}\n")
 
-    print(f"Olutvarasto: {olutta}")
-    print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
-    print(f"saatiin {saatiin}")
-    print(f"Olutvarasto: {olutta}")
+    print(f"Otetaan 1000.0 oluesta\nsaatiin {saatiin}\nOlutvarasto: {olutta}\n")
 
-    print(f"Mehuvarasto: {mehua}")
-    print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
-    print(f"saatiin {saatiin}")
-    print(f"Mehuvarasto: {mehua}")
-
+    print(f"Otetaan -32.9 mehusta\nsaatiin {saatiin}\nMehuvarasto: {mehua}")
 
 if __name__ == "__main__":
     main()
